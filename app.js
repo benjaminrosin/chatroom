6bb8adaecab4aba07996e88f5e7a8d30636cfb14
Benjamin Rosin <benjaminro@std.hac.ac.il>
2025-01-13 16:51:40 +0200

    photo

--- app.js
+++ app.js
@@ -20,7 +20,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', loginRouter);
+app.get('/', function(req, res, next) {
+  res.redirect('/login');
+});
+
+app.use('/login', loginRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
