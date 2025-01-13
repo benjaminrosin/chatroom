0e1a660a8cbd7ee1341bd52754b01fcfbbd65aec
Benjamin Rosin <benjaminro@std.hac.ac.il>
2025-01-13 18:11:24 +0200

    login and signup ejs forms

--- routes/signup.js
+++ routes/signup.js
@@ -3,7 +3,16 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('signup', { title: 'Signup' });
+    res.render('signup', { title: 'Signup' , startRegistration: true});
 });
+
+router.post('/', function(req, res, next) {
+    //check input and register
+    res.redirect('login')
+})
+
+router.post('/password', function(req, res, next) {
+    res.render('signup', { title: 'Signup' , startRegistration: false});
+})
 
 module.exports = router;
