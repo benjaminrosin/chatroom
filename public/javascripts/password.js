dfc36581fceac8000adfb6c46edb15b911032d20
Benjamin Rosin <benjaminro@std.hac.ac.il>
2025-01-15 15:18:13 +0200

    js

---
+++ public/javascripts/password.js
@@ -0,0 +1,11 @@
+
+const DOM = (function(){
+    document.addEventListener("DOMContentLoaded", function (){
+        const form = document.getElementById("passwordForm");
+        form.addEventListener("submit", function(e){
+            form.
+        })
+
+    })
+    return{};
+}());
