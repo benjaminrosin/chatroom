c59150beeb60d4b36092aa25be1b34f628f02879
Benjamin Rosin <benjaminro@std.hac.ac.il>
2025-01-15 16:00:08 +0200

    comparing passwords

--- public/javascripts/password.js
+++ public/javascripts/password.js
@@ -1,9 +1,25 @@
 
 const DOM = (function(){
-    document.addEventListener("DOMContentLoaded", function (){
+    document.addEventListener("DOMContentLoaded", function () {
+
         const form = document.getElementById("passwordForm");
-        form.addEventListener("submit", function(e){
-            form.
+
+        form.addEventListener("submit", function(event){
+            const passwords = Array.from(form.querySelectorAll('[type="password"]'));
+            const passwordA = passwords[0].value.trim();
+            const passwordB = passwords[1].value.trim();
+
+            console.log(passwordA, passwordB);
+
+            if(passwordA !== passwordB){
+                event.preventDefault();
+                passwords.forEach(function(field){field.classList.add('invalid');});
+
+                const errorMessage = form.querySelector(".text-danger");
+                if (errorMessage) {
+                    errorMessage.innerHTML = 'The passwords are not matching';
+                }
+            }
         })
 
     })
