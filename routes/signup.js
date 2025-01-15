4d73d07080bd6af7bbddb675287a97d0b5e19d1c
Benjamin Rosin <benjaminro@std.hac.ac.il>
2025-01-15 15:04:07 +0200

    logo and error msg

--- routes/signup.js
+++ routes/signup.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+let once = true;
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('signup', { title: 'Signup' , startRegistration: true});
+    res.render('signup', { title: 'Signup' , startRegistration: true, errorMsg: ''});
 });
 
 router.post('/', function(req, res, next) {
@@ -12,6 +14,10 @@ router.post('/', function(req, res, next) {
 })
 
 router.post('/password', function(req, res, next) {
+    if (once){
+        once = false;
+        res.render('signup', { title: 'Signup' , startRegistration: true , errorMsg: 'email already in use'});
+    }
     res.render('signup', { title: 'Signup' , startRegistration: false});
 })
 
